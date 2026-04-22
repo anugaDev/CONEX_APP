313137bd6b57cc32a0d6dab0f268fbdcf5367050
Adrián Núñez Garrido <adrinuga@gmail.com>
2026-04-22 13:35:18 +0200

    directory setup

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace CONEX_APP;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+    }
+}
---
+++ Presentation/Views/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace CONEX_APP.Presentation.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
